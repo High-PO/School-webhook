af3f99f8b0ac902748bccbdfe15e6073d3398de0
OSJ <promotion0402@gmail.com>
2022-04-23 11:27:08 +0900

    update source code

--- index.js
+++ index.js
@@ -7,27 +7,41 @@ exports.handler = async (event) => { // AWS Lambda Handler
     school.init(School.Type.<Your_School_Type>, School.Region.<Your_Region_Code>, result[0].schoolCode); // 학교 초기화(이걸 반드시 해야함)
     const meal = await school.getMeal(); // meal이라는 상수에 school.getMeal()을 넣어서 코드를 간소화
     var Today = meal.today // Today로 meal.today(오늘 급식)을 간소화
-    try{ //시도
-        const skfWk = await axios.post("<Your_WebHook_Url>", { // axios 모듈을 가지고 웹훅 주소로 post를 한다.
-            "content":`${meal.month}월 ${meal.day}일 급식입니다.` // post(채팅) 내용
-        });
-        console.info("날짜 확인 성공"); //성공시 AWS Lambda에 띄울 메세지
+    var not = "" // null 값 생성
+    if (Today == not ){
+        try{
+        const rmqtlrx = await axios.post("<Your_WebHook_Url>", { // axios 모듈을 가지고 웹훅 주소로 post를 한다.
+            "content":`${meal.month}월 ${meal.day}일 급식은 없거나 조회되지 않습니다.` //급식없는날 post
+            });
+            console.info("급식 없는 날"); // 콘솔에 띄운다.
+        }
+        catch(error){
+            console.error("급식 조회불가 부분 오류"); // 급식 조회이 없는날 부분에서 오류 발생시 뜸
+        }
     }
-    catch(eroor){ //에러 발생시
-        console.error("날짜 확인 실패", eroor); //에러 시  AWS Lambda에 '날짜 확인 실패' 라고 띄운다.
-    }    
-    try{ //시도
-        const rmqtlr = await axios.post("<Your_WebHook_Url>", { // 위와 같이 채팅을 사용한다.
-            "content":Today // Today => meal.today => school.getMeal() 이므로 아주 간소화 시킨 변수를 채팅으로 보낸다.
-        });
-        console.info("급식 조회 및 전송 성공"); //성공시 AWS Lambda에 띄울 내용
-    } 
-    catch(eroor){ //에러 발생시
-        console.error("급식 조회 및 전송 실패", eroor); // 에러시 AWS Lambda에 '급식 조회 및 전송 실패' 라고 띄운다.
+    else{
+        try{ //시도
+            const skfWk = await axios.post("<Your_WebHook_Url>", { // axios 모듈을 가지고 웹훅 주소로 post를 한다.
+                "content":`${meal.month}월 ${meal.day}일 급식입니다.` // post(채팅) 내용
+            });
+            console.info("날짜 확인 성공"); //성공시 AWS Lambda에 띄울 메세지
+        }
+        catch(eroor){ //에러 발생시
+            console.error("날짜 확인 실패", eroor); //에러 시  AWS Lambda에 '날짜 확인 실패' 라고 띄운다.
+        }    
+        try{ //시도
+            const rmqtlr = await axios.post("<Your_WebHook_Url>", { // 위와 같이 채팅을 사용한다.
+                "content":Today // Today => meal.today => school.getMeal() 이므로 아주 간소화 시킨 변수를 채팅으로 보낸다.
+            });
+            console.info("급식 조회 및 전송 성공"); //성공시 AWS Lambda에 띄울 내용
+        } 
+        catch(eroor){ //에러 발생시
+            console.error("급식 조회 및 전송 실패", eroor); // 에러시 AWS Lambda에 '급식 조회 및 전송 실패' 라고 띄운다.
+        }
+        const response = { // response라는 상수에 아래의 내용을 넣는다.
+            statusCode: 200, //성공 코드 200
+            body: JSON.stringify('Successfull! Good Job'), // 성공시 띄울 메세지
+        };
+        return response; //respose를 반환한다.
     }
-    const response = { // response라는 상수에 아래의 내용을 넣는다.
-        statusCode: 200, //성공 코드 200
-        body: JSON.stringify('Successfull! Good Job'), // 성공시 띄울 메세지
-    };
-    return response; //respose를 반환한다.
 };
